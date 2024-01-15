69301b2eae7a39f806aab7172b233695827f8332
Aaquib Ahmed <aaquib.ahmed@impactanalytics.co>
2024-01-15 23:01:53 +0530

    feat: two empty resolver created

---
+++ pagebuilder/src/Resolver/BuilderResolver/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BuilderResolver() {
+  return (
+    <div>BuilderResolver</div>
+  )
+}
---
+++ pagebuilder/src/Resolver/PageResolver/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageResolver() {
+  return (
+    <div>PageResolver</div>
+  )
+}
