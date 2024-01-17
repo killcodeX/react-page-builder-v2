69ae23204bb7065944f4af9e32a3588992d23ddc
Aaquib Ahmed <aaquib.ahmed@impactanalytics.co>
2024-01-17 20:51:45 +0530

    feat: json generator page is completed

--- pagebuilder/src/Modules/JsonGenerator/index.jsx
+++ pagebuilder/src/Modules/JsonGenerator/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
+import { Col, Row } from "antd";
+import ReactJson from "react-json-view";
+import { useSelector } from "react-redux";
 
 export default function JSONGenerator() {
+  let state = useSelector((state) => state.build.pageBuilder);
   return (
-    <div>jsonGenerator</div>
-  )
+    <section className="generator-builder">
+      <div className="generator-builder-heading">Json Generator</div>
+      <Row>
+        <Col span={24} className="row mh-100">
+          <ReactJson
+            theme="colors"
+            src={state}
+            style={{
+              padding: "16px",
+            }}
+          />
+        </Col>
+      </Row>
+    </section>
+  );
 }
