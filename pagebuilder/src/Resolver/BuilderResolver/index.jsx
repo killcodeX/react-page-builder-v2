fd4a74db307f92283fe5d569eeb5f0904a6c773c
Aaquib Ahmed <aaquib.ahmed@impactanalytics.co>
2024-01-17 21:59:51 +0530

    feat: drop box added

--- pagebuilder/src/Resolver/BuilderResolver/index.jsx
+++ pagebuilder/src/Resolver/BuilderResolver/index.jsx
@@ -1,34 +1,36 @@
 import react, { useState, useEffect } from "react";
 import "./style.css";
-import { useDrop } from 'react-dnd'
-import { Empty } from "antd";
+import { useDrop } from "react-dnd";
+import { Empty, Divider } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { addSection } from "../../Redux/Actions/actions";
 import Resolver from "./resolver";
 import EmptyStateImg from "../../Assets/emptystateimg.svg";
+import SectionDrop from "../../Assets/sectiondrop.svg";
+import { PlusOutlined } from "@ant-design/icons";
 
 export default function BuilderResolver() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const layersData = useSelector((state) => state);
   const [layers, setLayers] = useState(layersData.build.pageBuilder || []);
 
-  useEffect(()=>{
-    setLayers(layersData.build.pageBuilder)
-  },[layersData.build.pageBuilder])
+  useEffect(() => {
+    setLayers(layersData.build.pageBuilder);
+  }, [layersData.build.pageBuilder]);
 
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
-    accept: 'section',
+    accept: "section",
     drop: (item) => {
-        dispatch(addSection(item))
+      dispatch(addSection(item));
     },
-    canDrop: (item) => item.component === 'section',
+    canDrop: (item) => item.component === "section",
     collect: (monitor) => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop(),
     }),
-  }))
+  }));
 
-//   console.log(canDrop, isOver)
+  //   console.log(canDrop, isOver)
 
   if (layers.length == 0) {
     return (
@@ -36,18 +38,25 @@ export default function BuilderResolver() {
         <div className="drag-drop-empty-container" ref={drop}>
           <Empty
             image={EmptyStateImg}
-            description={<span>Drag & Drop a Section Component to start building UI</span>}
+            description={
+              <span>Drag & Drop a Section Component to start building UI</span>
+            }
           />
         </div>
       </section>
     );
   }
 
-  return <section className="builder-resolver">
-    {
-        layers.map(component => {
-            return <Resolver key={component.id} component={component}/>
-        })
-    }
-  </section>;
+  return (
+    <section className="builder-resolver">
+      {layers.map((component) => {
+        return <Resolver key={component.id} component={component} />;
+      })}
+      <Divider/>
+      <div className="drag-drop-section-container" ref={drop}>
+        {/* <PlusOutlined /> */}
+        <span>Drag & Drop more Section</span>
+      </div>
+    </section>
+  );
 }
--- pagebuilder/src/Resolver/BuilderResolver/layers.jsx
+++ pagebuilder/src/Resolver/BuilderResolver/layers.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Drawer } from "antd";
+import { useDrop } from "react-dnd";
+import { useSelector, useDispatch } from "react-redux";
+import { addSection } from "../../Redux/Actions/actions";
+import { EditOutlined } from "@ant-design/icons";
 
 export function Layers(layer) {
+  const dispatch = useDispatch();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: "section",
+    drop: (item) => {
+      dispatch(addSection(item));
+    },
+    canDrop: (item) => item.component === "section",
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }));
+
   switch (layer.component) {
     case "section":
       return (
-        <div className="page-builder-component-card" key={layer.id}>
-          <div className="page-builder-component-label">Section</div>
-          {/* <IconButton onClick={() => setDrawer(true)}>
-            <CiEdit />
-          </IconButton> */}
-          {/* <Drawer anchor="right" open={drawer} onClose={() => setDrawer(false)}>
+        <>
+          <div className="page-builder-component-card" key={layer.id}>
+            <div className="page-builder-component-label">Section</div>
+            <div className="page-builder-component-edit" onClick={() => setOpenDrawer(true)}>
+              <EditOutlined />
+            </div>
+            <Drawer title="Section Setting" onClose={() => setOpenDrawer(false)} open={openDrawer}>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </Drawer>
+            {/* <Drawer anchor="right" open={drawer} onClose={() => setDrawer(false)}>
             <Section
               drawer={drawer}
               setDrawer={setDrawer}
@@ -19,7 +44,11 @@ export function Layers(layer) {
               setSnackbarType={setSnackbarType}
             />
           </Drawer> */}
-        </div>
+          </div>
+          <div className="drag-drop-component-container" ref={drop}>
+            <span>Drag & Drop component</span>
+          </div>
+        </>
       );
   }
 }
